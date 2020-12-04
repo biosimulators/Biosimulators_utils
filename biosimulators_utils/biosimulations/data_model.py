bd472d75c36d9bfe6341ed1b66185095133b58f3
Jonathan Karr <jonrkarr@gmail.com>
2020-12-04 02:23:45 -0500

    adding data model for biosimulatons-related information

---
+++ biosimulators_utils/biosimulations/data_model.py
@@ -0,0 +1,228 @@
+from ..data_model import Person
+from ..utils import are_lists_equal
+
+__all__ = [
+    'Metadata',
+    'ExternalReferences',
+    'Citation',
+    'Identifier',
+    'OntologyTerm',
+]
+
+
+class Metadata(object):
+    """ Metadata about an object
+
+    Attributes:
+        description (:obj:`str`): description
+        tags (:obj:`list` of :obj:`str`): tags
+        authors (:obj:`list` of :obj:`Person`): authors
+        references (:obj:`ExternalReferences`): identifiers and citations
+        license (:obj:`OntologyTerm`): license
+    """
+
+    def __init__(self, description=None, tags=None, authors=None, references=None, license=None):
+        """
+        Args:
+            description (:obj:`str`, optional): description
+            tags (:obj:`list` of :obj:`str`, optional): tags
+            authors (:obj:`list` of :obj:`Person`, optional): authors
+            references (:obj:`ExternalReferences`, optional): identifiers and citations
+            license (:obj:`OntologyTerm`, optional): license
+        """
+        self.description = description
+        self.tags = tags or []
+        self.authors = authors or []
+        self.references = references
+        self.license = license
+
+    def to_tuple(self):
+        """ Get a tuple representation
+
+        Returns:
+            :obj:`tuple` of :obj:`str`: tuple representation
+        """
+        return (self.description, tuple(self.tags),
+                tuple(sorted(author.to_tuple() for author in self.authors)),
+                self.references.to_tuple() if self.references else None,
+                self.license.to_tuple() if self.license else None)
+
+    def is_equal(self, other):
+        """ Determine if metadata are equal
+
+        Args:
+            other (:obj:`Metadata`): another metadata
+
+        Returns:
+            :obj:`bool`: :obj:`True`, if two metadata are equal
+        """
+        return self.__class__ == other.__class__ \
+            and self.description == other.description \
+            and sorted(self.tags) == sorted(other.tags) \
+            and are_lists_equal(self.authors, other.authors) \
+            and ((self.references is None and self.references == other.references)
+                 or (self.references is not None and self.references.is_equal(other.references))) \
+            and ((self.license is None and self.license == other.license)
+                 or (self.license is not None and self.license.is_equal(other.license)))
+
+
+class ExternalReferences(object):
+    """ Identifiers and citations of an object
+
+    Attributes:
+        identifiers (:obj:`list` of :obj:`Identifier`): identifiers
+        citations (:obj:`list` of :obj:`Citation`): citations
+    """
+
+    def __init__(self, identifiers=None, citations=None):
+        """
+        Args:
+            identifiers (:obj:`list` of :obj:`Identifier`, optional): identifiers
+            citations (:obj:`list` of :obj:`Citation`, optional): citations
+        """
+        self.identifiers = identifiers or []
+        self.citations = citations or []
+
+    def to_tuple(self):
+        """ Get a tuple representation
+
+        Returns:
+            :obj:`tuple` of :obj:`str`: tuple representation
+        """
+        return (
+            tuple(sorted(identifier.to_tuple() for identifier in self.identifiers)),
+            tuple(sorted(citation.to_tuple() for citation in self.citations)),
+        )
+
+    def is_equal(self, other):
+        """ Determine if collections of external references are equal
+
+        Args:
+            other (:obj:`ExternalReferences`): another collection of external referencse
+
+        Returns:
+            :obj:`bool`: :obj:`True`, if two collections of external references are equal
+        """
+        return self.__class__ == other.__class__ \
+            and are_lists_equal(self.identifiers, other.identifiers) \
+            and are_lists_equal(self.citations, other.citations)
+
+
+class Citation(object):
+    """ A citation
+
+    Attributes:
+        title (:obj:`str`): title
+        authors (:obj:`str`): authors
+        journal (:obj:`str`): journal
+        volume (:obj:`str`): volume
+        issue (:obj:`str`): issue
+        pages (:obj:`str`): pages
+        year (:obj:`int`): year
+        identifiers (:obj:`list` of :obj:`Identifier`): identifiers
+    """
+
+    def __init__(self, title=None, authors=None, journal=None, volume=None, issue=None, pages=None, year=None, identifiers=None):
+        """
+        Args:
+            title (:obj:`str`, optional): title
+            authors (:obj:`str`, optional): authors
+            journal (:obj:`str`, optional): journal
+            volume (:obj:`str`, optional): volume
+            issue (:obj:`str`, optional): issue
+            pages (:obj:`str`, optional): pages
+            year (:obj:`int`, optional): year
+            identifiers (:obj:`list` of :obj:`Identifier`, optional): identifiers
+        """
+        self.title = title
+        self.authors = authors
+        self.journal = journal
+        self.volume = volume
+        self.issue = issue
+        self.pages = pages
+        self.year = year
+        self.identifiers = identifiers or []
+
+    def to_tuple(self):
+        """ Get a tuple representation
+
+        Returns:
+            :obj:`tuple` of :obj:`str`: tuple representation
+        """
+        return (
+            self.title,
+            self.authors,
+            self.journal,
+            self.volume,
+            self.issue,
+            self.pages,
+            self.year,
+            tuple(sorted(identifier.to_tuple() for identifier in self.identifiers)),
+        )
+
+    def is_equal(self, other):
+        """ Determine if citations are equal
+
+        Args:
+            other (:obj:`Citation`): another citation
+
+        Returns:
+            :obj:`bool`: :obj:`True`, if two citations are equal
+        """
+        return self.__class__ == other.__class__ \
+            and self.title == other.title \
+            and self.authors == other.authors \
+            and self.journal == other.journal \
+            and self.volume == other.volume \
+            and self.issue == other.issue \
+            and self.pages == other.pages \
+            and self.year == other.year \
+            and are_lists_equal(self.identifiers, other.identifiers)
+
+
+class Identifier(object):
+    """ An identifier
+
+    Attributes:
+        namespace (:obj:`str`): namespace
+        id (:obj:`str`): id
+        url (:obj:`str`): URL
+    """
+
+    def __init__(self, namespace=None, id=None, url=None):
+        """
+        Args:
+            namespace (:obj:`str`, optional): namespace
+            id (:obj:`str`, optional): id
+            url (:obj:`str`, optional): URL
+        """
+        self.namespace = namespace
+        self.id = id
+        self.url = url
+
+    def to_tuple(self):
+        """ Get a tuple representation
+
+        Returns:
+            :obj:`tuple` of :obj:`str`: tuple representation
+        """
+        return (self.namespace, self.id, self.url)
+
+    def is_equal(self, other):
+        """ Determine if identifiers are equal
+
+        Args:
+            other (:obj:`Identifier`): another identifier
+
+        Returns:
+            :obj:`bool`: :obj:`True`, if two identifiers are equal
+        """
+        return self.__class__ == other.__class__ \
+            and self.namespace == other.namespace \
+            and self.id == other.id \
+            and self.url == other.url
+
+
+class OntologyTerm(Identifier):
+    """ Term in an ontology """
+    pass
---
+++ tests/biosimulations/test_data_model.py
@@ -0,0 +1,122 @@
+import unittest
+from biosimulators_utils.biosimulations import data_model
+from biosimulators_utils.data_model import Person
+
+
+class DataModelTestCase(unittest.TestCase):
+    def test(self):
+        identifier1 = data_model.Identifier(namespace='KISAO', id='KISAO_0000029')
+        identifier2 = data_model.Identifier(namespace='KISAO', id='KISAO_0000029')
+        identifier3 = data_model.Identifier(namespace='KISAO', id='KISAO_0000028')
+        self.assertTrue(identifier1.is_equal(identifier2))
+        self.assertFalse(identifier1.is_equal(identifier3))
+
+        citation1 = data_model.Citation(title='title', authors='authors')
+        citation2 = data_model.Citation(title='title', authors='authors')
+        citation3 = data_model.Citation(title='title2', authors='authors2')
+        self.assertTrue(citation1.is_equal(citation2))
+        self.assertFalse(citation1.is_equal(citation3))
+
+        refs1 = data_model.ExternalReferences(
+            identifiers=[identifier1, identifier3],
+            citations=[citation1, citation3],
+        )
+        refs2 = data_model.ExternalReferences(
+            identifiers=[identifier1, identifier3],
+            citations=[citation1, citation3],
+        )
+        refs3 = data_model.ExternalReferences(
+            identifiers=[identifier1, identifier2],
+            citations=[citation1, citation3],
+        )
+        self.assertTrue(refs1.is_equal(refs2))
+        self.assertFalse(refs1.is_equal(refs3))
+        self.assertEqual(refs1.to_tuple(), (
+            (identifier3.to_tuple(), identifier1.to_tuple()),
+            (citation1.to_tuple(), citation3.to_tuple()),
+        ))
+
+        person1 = Person(given_name='first', family_name='last')
+        person2 = Person(given_name='first', family_name='last')
+        person3 = Person(given_name='first3', family_name='last3')
+
+        metadata1 = data_model.Metadata(
+            description="description",
+            tags=["tag1", "tag2"],
+            authors=[person1, person3],
+            references=refs1,
+            license=data_model.OntologyTerm(
+                namespace="SPDX",
+                id="MIT",
+            ))
+        metadata2 = data_model.Metadata(
+            description="description",
+            tags=["tag1", "tag2"],
+            authors=[person1, person3],
+            references=refs1,
+            license=data_model.OntologyTerm(
+                namespace="SPDX",
+                id="MIT",
+            ))
+        self.assertTrue(metadata1.is_equal(metadata2))
+
+        metadata3 = data_model.Metadata(
+            description="description2",
+            tags=["tag1", "tag2"],
+            authors=[person1, person3],
+            references=refs1,
+            license=data_model.OntologyTerm(
+                namespace="SPDX",
+                id="MIT",
+            ))
+        self.assertFalse(metadata1.is_equal(metadata3))
+
+        metadata3 = data_model.Metadata(
+            description="description",
+            tags=["tag2", "tag2"],
+            authors=[person1, person3],
+            references=refs1,
+            license=data_model.OntologyTerm(
+                namespace="SPDX",
+                id="MIT",
+            ))
+        self.assertFalse(metadata1.is_equal(metadata3))
+
+        metadata3 = data_model.Metadata(
+            description="description",
+            tags=["tag1", "tag2"],
+            authors=[person1, person1],
+            references=refs1,
+            license=data_model.OntologyTerm(
+                namespace="SPDX",
+                id="MIT",
+            ))
+        self.assertFalse(metadata1.is_equal(metadata3))
+
+        metadata3 = data_model.Metadata(
+            description="description",
+            tags=["tag1", "tag2"],
+            authors=[person1, person3],
+            references=refs3,
+            license=data_model.OntologyTerm(
+                namespace="SPDX",
+                id="MIT",
+            ))
+        self.assertFalse(metadata1.is_equal(metadata3))
+
+        metadata3 = data_model.Metadata(
+            description="description",
+            tags=["tag1", "tag2"],
+            authors=[person1, person3],
+            references=refs1,
+            license=data_model.OntologyTerm(
+                namespace="SPDX",
+                id="Apache-2.0",
+            ))
+        self.assertFalse(metadata1.is_equal(metadata3))
+
+        self.assertEqual(metadata1.to_tuple()[0], metadata1.description)
+        self.assertEqual(metadata1.to_tuple()[1], tuple(metadata1.tags))
+        self.assertEqual(metadata1.to_tuple()[2], tuple([person1.to_tuple(), person3.to_tuple()]))
+        self.assertEqual(metadata1.to_tuple()[3], tuple(refs1.to_tuple(),))
+        self.assertEqual(metadata1.to_tuple()[4], metadata1.license.to_tuple())
