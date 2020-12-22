14f1fed1474d25343d076d8de46550c68cb9b58e
Jonathan Karr <jonrkarr@gmail.com>
2020-12-22 00:46:11 -0500

    adding method to get ids of reports

--- biosimulators_utils/report/io.py
+++ biosimulators_utils/report/io.py
@@ -8,8 +8,10 @@
 
 from ..config import get_config
 from .data_model import ReportFormat
+import glob
 import os
 import pandas
+import tables
 
 __all__ = [
     'ReportWriter',
@@ -66,7 +68,7 @@ class ReportWriter(object):
 class ReportReader(object):
     """ Class for reading reports of simulation results """
 
-    def run(self, base_path, rel_path, format=ReportFormat.csv):
+    def run(self, base_path, rel_path, format=ReportFormat.h5):
         """ Read a report for a file
 
         Args:
@@ -98,6 +100,40 @@ class ReportReader(object):
             return pandas.read_hdf(filename,
                                    key=rel_path,
                                    )
+
+        else:
+            raise NotImplementedError('Report format {} is not supported'.format(format))
+
+    def get_ids(self, base_path, format=ReportFormat.h5):
+        """ Get the ids of the reports in a file
+
+        Args:
+            base_path (:obj:`str`): path to save results
+
+                * CSV: parent directory to save results
+                * HDF5: file to save results
+
+            format (:obj:`ReportFormat`, optional): report format
+
+        Returns:
+            :obj:`set` of :obj:`str`: ids of reports
+        """
+        if format == ReportFormat.csv:
+            report_ids = set()
+            for path in glob.glob(os.path.join(base_path, '**/*.' + format.value), recursive=True):
+                report_ids.add(os.path.relpath(path, base_path)[0:-len(format.value)-1])
+            return report_ids
+
+        elif format == ReportFormat.h5:
+            filename = os.path.join(base_path, get_config().H5_REPORTS_PATH)
+            reports_file = tables.open_file(filename, mode="r")
+            report_ids = set()
+            for node in reports_file.walk_nodes():
+                base_path, _, rel_path = node._v_pathname.rpartition('/')
+                if rel_path == 'table':
+                    report_ids.add(base_path[1:])
+            reports_file.close()
+            return report_ids
 
         else:
             raise NotImplementedError('Report format {} is not supported'.format(format))
--- tests/report/test_report_io.py
+++ tests/report/test_report_io.py
@@ -1,9 +1,20 @@
 from biosimulators_utils.report import data_model
 from biosimulators_utils.report import io
+import numpy
+import os
+import pandas
+import shutil
+import tempfile
 import unittest
 
 
 class ReportIoTestCase(unittest.TestCase):
+    def setUp(self):
+        self.dirname = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.dirname)
+
     def test_write_errors(self):
         with self.assertRaisesRegex(NotImplementedError, 'is not supported'):
             io.ReportWriter().run(None, None, None, format='TSV')
@@ -11,3 +22,25 @@ class ReportIoTestCase(unittest.TestCase):
     def test_read_errors(self):
         with self.assertRaisesRegex(NotImplementedError, 'is not supported'):
             io.ReportReader().run(None, None, format='TSV')
+
+    def test_get_ids(self):
+        data = numpy.array([[1, 2, 3], [4, 5, 6]])
+        df = pandas.DataFrame(data, index=['A', 'B'])
+
+        for format in [data_model.ReportFormat.h5, data_model.ReportFormat.csv]:
+            filename = os.path.join(self.dirname, 'test')
+            io.ReportWriter().run(df, filename, 'a/b/c.sedml/report1', format=format)
+            io.ReportWriter().run(df, filename, 'a/b/c.sedml/report2', format=format)
+            io.ReportWriter().run(df, filename, 'a/b/c.sedml/report3', format=format)
+            io.ReportWriter().run(df, filename, 'a/b/d.sedml/report4', format=format)
+            io.ReportWriter().run(df, filename, 'a/b/report5', format=format)
+            io.ReportWriter().run(df, filename, 'a/b/report6', format=format)
+
+            self.assertEqual(io.ReportReader().get_ids(filename, format=format), set([
+                'a/b/c.sedml/report1',
+                'a/b/c.sedml/report2',
+                'a/b/c.sedml/report3',
+                'a/b/d.sedml/report4',
+                'a/b/report5',
+                'a/b/report6',
+            ]))
