4326166276040a98f7c1a06728850491737bba5d
Jonathan Karr <jonrkarr@gmail.com>
2021-04-15 17:19:24 -0400

    starting new microversion; adding option to 'get_parameters_variables_for_simulation'

--- biosimulators_utils/sedml/model_utils.py
+++ biosimulators_utils/sedml/model_utils.py
@@ -14,7 +14,7 @@ import types  # noqa: F401
 __all__ = ['get_parameters_variables_for_simulation']
 
 
-def get_parameters_variables_for_simulation(model_filename, model_language, simulation_type, algorithm):
+def get_parameters_variables_for_simulation(model_filename, model_language, simulation_type, algorithm, **model_language_options):
     """ Get the possible observables for a simulation of a model
 
     This method supports the following formats
@@ -29,6 +29,7 @@ def get_parameters_variables_for_simulation(model_filename, model_language, simu
         simulation_type (:obj:`types.Type`): subclass of :obj:`Simulation`
         algorithm (:obj:`str`): KiSAO id of the algorithm for simulating the model (e.g., ``KISAO_0000019``
             for CVODE)
+        **model_language_options: additional options to pass to the methods for individual model formats
 
     Returns:
         :obj:`list` of :obj:`ModelAttributeChange`: possible attributes of a model that can be changed and their default values
@@ -40,7 +41,9 @@ def get_parameters_variables_for_simulation(model_filename, model_language, simu
     if re.match(ModelLanguagePattern.SBML.value, model_language):
         import biosimulators_utils.sbml.utils  # imported here to only import libraries for required model languages
         return biosimulators_utils.sbml.utils.get_parameters_variables_for_simulation(
-            model_filename, model_language, simulation_type, algorithm)
+            model_filename, model_language, simulation_type, algorithm,
+            **model_language_options,
+        )
 
     else:
         raise NotImplementedError(
