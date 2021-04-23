2f6d1c73f74016932585c19743f2a1ea1da2b497
Jonathan Karr <jonrkarr@gmail.com>
2021-04-23 16:30:50 -0400

    adding methods to determine if a simulator can execute a SED document

--- biosimulators_utils/simulator/specs.py
+++ biosimulators_utils/simulator/specs.py
@@ -6,9 +6,169 @@
 :License: MIT
 """
 
-from ..sedml.data_model import Algorithm, AlgorithmParameterChange
+from ..sedml.data_model import (SedDocument, Task, ModelLanguagePattern, ModelLanguageEdamId,  # noqa: F401
+                                SteadyStateSimulation, UniformTimeCourseSimulation, Algorithm, AlgorithmParameterChange)
 from .data_model import SoftwareInterface
 import json
+import natsort
+import re
+import requests
+
+__all__ = [
+    'BIOSIMULATORS_API_ENDPOINT',
+    'get_simulator_specs',
+    'does_simulator_have_capabilities_to_execute_sed_document',
+    'does_simulator_have_capabilities_to_execute_sed_task',
+    'does_algorithm_implementation_have_capabilities_to_execute_sed_task',
+    'does_algorithm_implementation_have_capabilities_to_execute_sed_model_language',
+    'does_algorithm_implementation_have_capabilities_to_execute_parameter',
+    'gen_algorithms_from_specs',
+]
+
+
+BIOSIMULATORS_API_ENDPOINT = 'https://api.biosimulators.org/simulators'
+
+
+def get_simulator_specs(id, version='latest'):
+    """ Get the specifications of a simulation tool from the BioSimulators registry
+
+    Args:
+        id (:obj:`str`): id
+        version (:obj:`str`, optional): version
+
+    Returns:
+        :obj:`dict` with schema ``https://api.biosimulators.org/openapi.json#/components/schemas/Simulator``: specifications
+    """
+    if version == 'latest':
+        url = BIOSIMULATORS_API_ENDPOINT + '/' + id
+        response = requests.get(url)
+        response.raise_for_status()
+        version_specs = natsort.natsorted(response.json(), key=lambda version_spec: version_spec['version'])
+        return version_specs[-1]
+    else:
+        url = BIOSIMULATORS_API_ENDPOINT + '/' + id + '/' + version
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+
+
+def does_simulator_have_capabilities_to_execute_sed_document(sed_doc, simulator_specs):
+    """ Determine if a simulator has the capabilities to execute a SED document
+
+    Args:
+        sed_doc (:obj:`SedDocument): SED document
+        simulator_specs (:obj:`dict` with schema ``https://api.biosimulators.org/openapi.json#/components/schemas/Simulator``):
+            specifications of a simulation tool
+
+    Returns:
+        :obj:`bool`: whether the simulator has the capabilities to execute the SED document
+    """
+    for task in sed_doc.tasks:
+        if not does_simulator_have_capabilities_to_execute_sed_task(task, simulator_specs):
+            return False
+    return True
+
+
+def does_simulator_have_capabilities_to_execute_sed_task(task, simulator_specs):
+    """ Determine if a simulator has the capabilities to execute a SED task
+
+    Args:
+        task (:obj:`Task`): SED task
+        simulator_specs (:obj:`dict` with schema ``https://api.biosimulators.org/openapi.json#/components/schemas/Simulator``):
+            specifications of a simulation tool
+
+    Returns:
+        :obj:`bool`: whether the simulator has the capabilities to execute the SED task
+    """
+    if isinstance(task, Task):
+        for alg_specs in simulator_specs['algorithms']:
+            if does_algorithm_implementation_have_capabilities_to_execute_sed_task(task, alg_specs):
+                return True
+
+        return False
+
+    else:
+        for sub_task in task.sub_tasks:
+            if not does_simulator_have_capabilities_to_execute_sed_task(sub_task.task, simulator_specs):
+                return False
+        return True
+
+
+def does_algorithm_implementation_have_capabilities_to_execute_sed_task(task, algorithm_specs):
+    """ Determine if an implementation of an algorithm has the capabilities to execute a SED task
+
+    Args:
+        task (:obj:`Task`): SED task
+        algorithm_specs (:obj:`dict` with schema ``https://api.biosimulators.org/openapi.json#/components/schemas/Algorithm``):
+            specifications of the implementation of an algorithm
+
+    Returns:
+        :obj:`bool`: whether the implementation of the algorithm has the capabilities to execute the SED task
+    """
+    model = task.model
+    simulation = task.simulation
+    algorithm = simulation.algorithm
+
+    if algorithm_specs['kisaoId']['id'] == algorithm.kisao_id:
+        # check if the implementation supports the model language
+        if not does_algorithm_implementation_have_capabilities_to_execute_sed_model_language(model.language, algorithm_specs):
+            return False
+
+        # check if implementation supports the parameters of the algorithm
+        supports_parameters = True
+        for change in algorithm.changes:
+            if not does_algorithm_implementation_have_capabilities_to_execute_parameter(change.kisao_id, algorithm_specs):
+                supports_parameters = False
+                break
+
+        if not supports_parameters:
+            return False
+
+        return True
+
+    return False
+
+
+def does_algorithm_implementation_have_capabilities_to_execute_sed_model_language(model_language, algorithm_specs):
+    """ Determine if an implementation of an algorithm has the capabilities to execute a model langugae
+
+    Args:
+        model_language (:obj:`str`): SED URN for model language
+        algorithm_specs (:obj:`dict` with schema ``https://api.biosimulators.org/openapi.json#/components/schemas/Algorithm``):
+            specifications of the implementation of an algorithm
+
+    Returns:
+        :obj:`bool`: whether the implementation of the algorithm has the capabilities to execute the SED model language
+    """
+    model_language_edam_id = None
+    for model_language_pattern in ModelLanguagePattern.__members__.values():
+        if re.match(model_language_pattern.value, model_language):
+            model_language_edam_id = ModelLanguageEdamId[model_language_pattern.name].value
+            break
+    if not model_language_edam_id:
+        return False
+
+    for model_format_specs in algorithm_specs['modelFormats']:
+        if model_format_specs['id'] == model_language_edam_id:
+            return True
+    return False
+
+
+def does_algorithm_implementation_have_capabilities_to_execute_parameter(parameter_kisao_id, algorithm_specs):
+    """ Determine if an implementation of an algorithm has the capabilities to execute a model langugae
+
+    Args:
+        parameter_kisao_id (:obj:`str`): KiSAO id for an algorithm parameter
+        algorithm_specs (:obj:`dict` with schema ``https://api.biosimulators.org/openapi.json#/components/schemas/Algorithm``):
+            specifications of the implementation of an algorithm
+
+    Returns:
+        :obj:`bool`: whether the implementation of the algorithm has the capabilities to execute the SED parameter
+    """
+    for parameter_specs in algorithm_specs['parameters']:
+        if parameter_specs['kisaoId']['id'] == parameter_kisao_id:
+            return True
+    return False
 
 
 def gen_algorithms_from_specs(specifications):
--- tests/simulator/test_simulator_specs.py
+++ tests/simulator/test_simulator_specs.py
@@ -1,11 +1,90 @@
-from biosimulators_utils.sedml.data_model import Algorithm, AlgorithmParameterChange
-from biosimulators_utils.simulator.specs import gen_algorithms_from_specs
+from biosimulators_utils.sedml.data_model import (
+    SedDocument, Task, RepeatedTask, SubTask, Model, ModelLanguage,
+    UniformTimeCourseSimulation, Algorithm, AlgorithmParameterChange,
+)
+from biosimulators_utils.simulator.specs import (
+    get_simulator_specs,
+    does_simulator_have_capabilities_to_execute_sed_document,
+    gen_algorithms_from_specs,
+)
+import copy
 import os
 import unittest
 
 
 class SimulatorSpecsTestCase(unittest.TestCase):
-    def test(self):
+
+    def test_get_simulator_specs(self):
+        specs = get_simulator_specs('copasi', 'latest')
+        self.assertEqual(specs['id'], 'copasi')
+        self.assertIsInstance(specs['algorithms'], list)
+        for alg_specs in specs['algorithms']:
+            self.assertIsInstance(alg_specs, dict)
+
+        self.assertEqual(specs['version'], '4.30.240')
+
+        specs = get_simulator_specs('copasi', '4.30.240')
+        self.assertEqual(specs['id'], 'copasi')
+        self.assertIsInstance(specs['algorithms'], list)
+        for alg_specs in specs['algorithms']:
+            self.assertIsInstance(alg_specs, dict)
+
+    def test_does_simulator_have_capabilities_to_execute_sed_document(self):
+        specs = get_simulator_specs('tellurium', 'latest')
+        doc = SedDocument(
+            tasks=[
+                Task(
+                    model=Model(
+                        language=ModelLanguage.SBML.value,
+                    ),
+                    simulation=UniformTimeCourseSimulation(
+                        algorithm=Algorithm(
+                            kisao_id='KISAO_0000019',
+                            changes=[
+                                AlgorithmParameterChange(kisao_id='KISAO_0000209')
+                            ]
+                        )
+                    )
+                )
+            ]
+        )
+
+        self.assertTrue(does_simulator_have_capabilities_to_execute_sed_document(doc, specs))
+
+        doc2 = copy.deepcopy(doc)
+        doc2.tasks.append(RepeatedTask(
+            sub_tasks=[
+                SubTask(task=copy.deepcopy(doc2.tasks[0]))
+            ],
+        ))
+        self.assertTrue(does_simulator_have_capabilities_to_execute_sed_document(doc2, specs))
+
+        doc2 = copy.deepcopy(doc)
+        doc2.tasks.append(RepeatedTask(
+            sub_tasks=[
+                SubTask(task=copy.deepcopy(doc2.tasks[0]))
+            ],
+        ))
+        doc2.tasks[1].sub_tasks[0].task.model.language = ModelLanguage.CellML.value
+        self.assertFalse(does_simulator_have_capabilities_to_execute_sed_document(doc2, specs))
+
+        doc2 = copy.deepcopy(doc)
+        doc2.tasks[0].model.language = ModelLanguage.CellML.value
+        self.assertFalse(does_simulator_have_capabilities_to_execute_sed_document(doc2, specs))
+
+        doc2 = copy.deepcopy(doc)
+        doc2.tasks[0].model.language = 'urn:sedml:language:unknown'
+        self.assertFalse(does_simulator_have_capabilities_to_execute_sed_document(doc2, specs))
+
+        doc2 = copy.deepcopy(doc)
+        doc2.tasks[0].simulation.algorithm.kisao_id = 'KISAO_0000209'
+        self.assertFalse(does_simulator_have_capabilities_to_execute_sed_document(doc2, specs))
+
+        doc2 = copy.deepcopy(doc)
+        doc2.tasks[0].simulation.algorithm.changes[0].kisao_id = 'KISAO_0000019'
+        self.assertFalse(does_simulator_have_capabilities_to_execute_sed_document(doc2, specs))
+
+    def test_gen_algorithms_from_specs(self):
         algs = gen_algorithms_from_specs(os.path.join(os.path.dirname(__file__), '..', 'fixtures', 'tellurium.json'))
 
         self.assertEqual(len(algs), 5)
