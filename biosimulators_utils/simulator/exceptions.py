e4c04581d0b711b6837795d958c0628f3a5615ee
Jonathan Karr <jonrkarr@gmail.com>
2021-04-29 02:00:11 -0400

    refactoring to KiSAO

--- biosimulators_utils/simulator/exceptions.py
+++ biosimulators_utils/simulator/exceptions.py
@@ -10,15 +10,9 @@ from ..exceptions import BioSimulatorsException
 
 __all__ = [
     'AlgorithmDoesNotSupportModelFeatureException',
-    'AlgorithmCannotBeSubstitutedException',
 ]
 
 
 class AlgorithmDoesNotSupportModelFeatureException(BioSimulatorsException):
     """ Exception that an algorithm does not support a feature of a model """
-    pass  # pragma: no cover
-
-
-class AlgorithmCannotBeSubstitutedException(BioSimulatorsException):
-    """ Exception that the algorithm substitution policy does not allow an algorithm to be substituted """
     pass  # pragma: no cover
--- tests/simulator/test_simulator_warnings.py
+++
@@ -1,8 +0,0 @@
-from biosimulators_utils.simulator.warnings import AlgorithmSubstitutedWarning
-from biosimulators_utils.warnings import warn
-import unittest
-
-
-class SimulatorWarningsTestCase(unittest.TestCase):
-    def test(self):
-        warn('Alternate algorithm used', AlgorithmSubstitutedWarning)
