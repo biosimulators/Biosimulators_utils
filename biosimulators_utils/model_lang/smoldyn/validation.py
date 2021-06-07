38cd5322209dbb69486456cfee9f98075194f87f
Jonathan Karr <jonrkarr@gmail.com>
2021-06-07 14:52:31 -0400

    adding validation for Smoldyn

---
+++ biosimulators_utils/model_lang/smoldyn/validation.py
@@ -0,0 +1,61 @@
+""" Utilities for validating Smoldyn models
+
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2021-04-13
+:Copyright: 2021, Center for Reproducible Biomedical Modeling
+:License: MIT
+"""
+
+
+from ...log.data_model import StandardOutputErrorCapturerLevel
+from ...log.utils import StandardOutputErrorCapturer
+from smoldyn.biosimulators.combine import (
+    read_smoldyn_simulation_configuration,
+    disable_smoldyn_graphics_in_simulation_configuration,
+    write_smoldyn_simulation_configuration,
+    init_smoldyn_simulation_from_configuration_file,
+)
+import os
+import tempfile
+
+
+def validate_model(filename, name=None):
+    """ Check that a model is valid
+
+    Args:
+        filename (:obj:`str`): path to model
+        name (:obj:`str`, optional): name of model for use in error messages
+
+    Returns:
+        :obj:`tuple`:
+
+            * nested :obj:`list` of :obj:`str`: nested list of errors (e.g., required ids missing or ids not unique)
+            * nested :obj:`list` of :obj:`str`: nested list of errors (e.g., required ids missing or ids not unique)
+    """
+    errors = []
+    warnings = []
+
+    if filename:
+        if os.path.isfile(filename):
+            config = read_smoldyn_simulation_configuration(filename)
+            disable_smoldyn_graphics_in_simulation_configuration(config)
+            fid, config_filename = tempfile.mkstemp(suffix='.txt')
+            os.close(fid)
+            write_smoldyn_simulation_configuration(config, config_filename)
+            with StandardOutputErrorCapturer(level=StandardOutputErrorCapturerLevel.c, relay=False) as captured:
+                try:
+                    init_smoldyn_simulation_from_configuration_file(config_filename)
+                    valid = True
+                except ValueError:
+                    valid = False
+            if not valid:
+                errors.append(['`{}` is not a valid Smoldyn configuration file.'.format(filename), [[captured.get_text()]]])
+            os.remove(config_filename)
+
+        else:
+            errors.append(['`{}` is not a file.'.format(filename or '')])
+
+    else:
+        errors.append(['`filename` must be a path to a file, not `{}`.'.format(filename or '')])
+
+    return (errors, warnings)
---
+++ tests/model_lang/smoldyn/test_smoldyn_validation.py
@@ -0,0 +1,25 @@
+from biosimulators_utils.model_lang.smoldyn.validation import validate_model
+from biosimulators_utils.utils.core import flatten_nested_list_of_strings
+import os
+import unittest
+
+
+class SmoldynValidationTestCase(unittest.TestCase):
+    FIXTURE_DIR = os.path.join(os.path.dirname(__file__), '..', '..', 'fixtures', 'smoldyn')
+
+    def test(self):
+        errors, warnings = validate_model(os.path.join(self.FIXTURE_DIR, 'bounce1.txt'))
+        self.assertEqual(errors, [])
+        self.assertEqual(warnings, [])
+
+        errors, warnings = validate_model(os.path.join(self.FIXTURE_DIR, 'invalid.txt'))
+        self.assertIn("statement not recognized", flatten_nested_list_of_strings(errors))
+        self.assertEqual(warnings, [])
+
+        errors, warnings = validate_model(os.path.join(self.FIXTURE_DIR, 'not a file.txt'))
+        self.assertIn('is not a file', flatten_nested_list_of_strings(errors))
+        self.assertEqual(warnings, [])
+
+        errors, warnings = validate_model(None)
+        self.assertIn('must be a path', flatten_nested_list_of_strings(errors))
+        self.assertEqual(warnings, [])
