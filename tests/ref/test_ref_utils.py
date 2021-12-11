4d14a4245e23e99f447e8d016445862dd5de228f
Jonathan Karr <jonrkarr@gmail.com>
2021-12-11 15:20:41 -0500

    refactor: improved error handling

--- tests/ref/test_ref_utils.py
+++ tests/ref/test_ref_utils.py
@@ -1,5 +1,7 @@
 from biosimulators_utils.ref import utils
+from unittest import mock
 import Bio.Entrez
+import ftplib
 import os
 import shutil
 import tempfile
@@ -14,6 +16,25 @@ class RefUtilsTestCase(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree(self.dirname)
+
+    def test_search_entrez_records(self):
+        self.assertEqual(utils.search_entrez_records('pmc', '23184105[pmid]')['IdList'], ['5813803'])
+
+        with self.assertRaisesRegex(TypeError, 'must be a string'):
+            utils.search_entrez_records('pmc', None)
+
+    def test_get_entrez_record(self):
+        self.assertEqual(utils.get_entrez_record('pmc', '5813803')['Id'], '5813803')
+
+        with self.assertRaisesRegex(TypeError, 'must be a string'):
+            utils.get_entrez_record('pmc', None)
+
+    def test_get_pubmed_central_id(self):
+        self.assertEqual(utils.get_pubmed_central_id('23184105'), 'PMC5813803')
+        self.assertEqual(utils.get_pubmed_central_id('1234'), None)
+        self.assertEqual(utils.get_pubmed_central_id(1234), None)
+        self.assertEqual(utils.get_pubmed_central_id('abc'), None)
+        self.assertEqual(utils.get_pubmed_central_id(None), None)
 
     def test_get_reference_from_pubmed(self):
         ref = utils.get_reference_from_pubmed('1234')
@@ -34,6 +55,9 @@ class RefUtilsTestCase(unittest.TestCase):
         ref = utils.get_reference_from_pubmed(pubmed_id=None, doi='10.1103/PhysRevLett.127.104301x')
         self.assertEqual(ref, None)
 
+        ref = utils.get_reference_from_pubmed(None, None)
+        self.assertEqual(ref, None)
+
         with self.assertRaises(ValueError):
             utils.get_reference_from_pubmed('000')
 
@@ -45,8 +69,23 @@ class RefUtilsTestCase(unittest.TestCase):
         self.assertEqual(ref.doi, '10.1542/peds.2012-2758')
 
     def test_get_reference(self):
+        authors = [
+            'James R. Roberts',
+            'Catherine J. Karr',
+            'Jerome A. Paulson',
+            'Alice C. Brock-Utne',
+            'Heather L. Brumberg',
+            'Carla C. Campbell',
+            'Bruce P. Lanphear',
+            'Kevin C. Osterhoudt',
+            'Megan T. Sandel',
+            'Leonardo Trasande',
+            'Robert O. Wright',
+            'COUNCIL ON ENVIRONMENTAL HEALTH',
+        ]
+
         ref = utils.get_reference(pubmed_id='23184105')
-        self.assertEqual(ref.authors, ['J. R. Roberts', 'C. J. Karr', 'COUNCIL ON ENVIRONMENTAL HEALTH'])
+        self.assertEqual(ref.authors, authors)
         self.assertEqual(ref.title, 'Pesticide exposure in children')
         self.assertEqual(ref.year, '2012')
         self.assertEqual(ref.pubmed_id, '23184105')
@@ -54,7 +93,7 @@ class RefUtilsTestCase(unittest.TestCase):
         self.assertEqual(ref.doi, '10.1542/peds.2012-2758')
 
         ref = utils.get_reference(doi='10.1542/peds.2012-2758')
-        self.assertEqual(ref.authors, ['J. R. Roberts', 'C. J. Karr', 'COUNCIL ON ENVIRONMENTAL HEALTH'])
+        self.assertEqual(ref.authors, authors)
         self.assertEqual(ref.title, 'Pesticide exposure in children')
         self.assertEqual(ref.year, '2012')
         self.assertEqual(ref.pubmed_id, '23184105')
@@ -72,8 +111,17 @@ class RefUtilsTestCase(unittest.TestCase):
         images = utils.get_pubmed_central_open_access_graphics('PMC6684142', self.dirname)
         self.assertEqual(len(images), 2)
 
-        print(images[0].id)
         self.assertEqual(images[0].id, '10.1177_2473974X19861567/fig1-2473974X19861567')
         self.assertEqual(images[0].label, 'Figure 1')
         self.assertTrue(images[0].caption.startswith('<p xmlns:xlink'))
         self.assertTrue(os.path.isfile(images[0].filename))
+
+    def test_download_pubmed_central_record(self):
+        tgz_filename = os.path.join(self.dirname, 'record.tar.gz')
+
+        def save_tgz_file(_, writer):
+            pass
+
+        with self.assertRaisesRegex(Exception, 'could not be downloaded'):
+            with mock.patch.object(ftplib.FTP, 'retrbinary', side_effect=save_tgz_file):
+                utils.download_pubmed_central_record('PMC6684142', '', tgz_filename, self.dirname)
