d264b78792ba89f24c9c5bda43d437b1b03c1cbb
biosimulatorsdaemon <biosimulators.daemon@gmail.com>
2022-01-10 16:22:01 +0000

    chore: Updating compiled documentation

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.156',
+    VERSION: '0.1.157',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
