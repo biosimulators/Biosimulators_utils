5366ee65de1ae92f0dd727f565a7240345bb9654
Lucian Smith <lpsmith@uw.edu>
2023-06-21 16:11:23 -0700

    Yet another attempt to get the exception message.

--- biosimulators_utils/sedml/validation.py
+++ biosimulators_utils/sedml/validation.py
@@ -1771,9 +1771,8 @@ def validate_calculation(calculation):
 
         try:
             compiled_math = compile_math(calculation.math)
-        except TypeError as e:
-            errors.append(['The mathematical expression must be a `string`, not a `{}`.'.format(calculation.math.__class__)])
-            errors.append(e.what())
+        except TypeError as exception:
+            errors.append(['The mathematical expression must be a `string`, not a `{}`:'.format(calculation.math.__class__), [[str(exception)]]])
             return (errors, warnings)
         except (SyntaxError, CompilationException) as exception:
             errors.append(['The syntax of the mathematical expression `{}` is invalid.'.format(calculation.math), [[str(exception)]]])
