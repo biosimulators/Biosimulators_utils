c911bf81686af9368337540b200a64597b6ccd38
biosimulatorsdaemon <biosimulators.daemon@gmail.com>
2023-06-28 22:23:23 +0000

    chore: Updated compiled documentation [skip ci]

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.181',
+    VERSION: '0.1.182',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
