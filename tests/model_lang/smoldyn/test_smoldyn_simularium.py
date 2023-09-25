0b1d883fb5a4295ac1f75ff65d7261662cce7677
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-09-25 08:01:35 -0400

    updated test file

--- tests/model_lang/smoldyn/test_smoldyn_simularium.py
+++ tests/model_lang/smoldyn/test_smoldyn_simularium.py
@@ -1,9 +1,7 @@
 import os
 from biosimulators_utils.model_lang.smoldyn.utils import generate_new_simularium_file
 from biosimulators_utils.model_lang.smoldyn.simularium_converter import SmoldynDataConverter, CombineArchive
-from biosimulators_utils.viz.io import write_plot_3d
-from biosimulators_utils.report.data_model import DataGeneratorResults
-from biosimulators_utils.sedml.data_model import Plot3D
+
 
 ARCHIVE_NAME = 'minE_Andrews_0523'
 ARCHIVE_PATH = os.path.join('tests', 'fixtures', 'smoldyn', ARCHIVE_NAME)
@@ -13,14 +11,12 @@ SIMULARIUM_FILE = os.path.join(ARCHIVE_PATH, f'{ARCHIVE_NAME}')
 def test_plot_3d_from_df():
     archive = CombineArchive(rootpath=ARCHIVE_PATH, simularium_filename=SIMULARIUM_FILE)
     converter = SmoldynDataConverter(archive)
-    df = converter.read_model_output_dataframe()
 
-    # Sub-method for plotting to 3d
-    print(df)
+    # NOTE: Add Sub-method for plotting to 3d
+    df = converter.read_model_output_dataframe()
     data_generators = list(set(df['mol_name'].tolist()))
+    print(df)
     print(data_generators)
-    plot_type = Plot3D()
-    # plot = write_plot_3d(plot_type,
 
 
 if __name__ == '__main__':
