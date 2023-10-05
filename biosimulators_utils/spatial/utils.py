6d829417830414d83085bbfcbef14f62fd54fb25
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2023-10-05 08:04:48 -0400

    chore: updated utils subdir docstrings

--- biosimulators_utils/spatial/utils.py
+++ biosimulators_utils/spatial/utils.py
@@ -7,16 +7,18 @@
 """
 
 
+from typing import Union
 from biosimulators_utils.model_lang.smoldyn.validation import validate_model
-from biosimulators_utils.spatial.data_model import SpatialCombineArchive, ModelValidation
+from biosimulators_utils.spatial.data_model import SpatialCombineArchive, SmoldynCombineArchive, ModelValidation
 
 
-def generate_model_validation_object(archive: SpatialCombineArchive) -> ModelValidation:
+def generate_model_validation_object(archive: Union[SpatialCombineArchive, SmoldynCombineArchive]) -> ModelValidation:
     """ Generate an instance of `ModelValidation` based on the output of `archive.model_path`
             with above `validate_model` method.
 
     Args:
-        archive: (:obj:`SpatialCombineArchive`): Instance of `SpatialCombineArchive` to generate model validation on.
+        archive (:obj:`Union[SpatialCombineArchive, SmoldynCombineArchive]`): Instance of `SpatialCombineArchive`
+            by which to generate model validation on.
 
     Returns:
         :obj:`ModelValidation`
@@ -26,5 +28,13 @@ def generate_model_validation_object(archive: SpatialCombineArchive) -> ModelVal
     return validation
 
 
-def verify_simularium_in_archive(archive: SpatialCombineArchive) -> bool:
+def verify_simularium_in_archive(archive: Union[SpatialCombineArchive, SmoldynCombineArchive]) -> bool:
+    """Verify the presence of a simularium file within the passed `archive.paths` dict values.
+
+        Args:
+            archive: archive to search for simularium file.
+
+        Returns:
+            `bool`: Whether there exists a simularium file in the directory.
+    """
     return '.simularium' in list(archive.paths.keys())
